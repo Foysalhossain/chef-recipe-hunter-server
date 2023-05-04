b46901782dccec264a3c9fda5fe5defca63fe0ce
Foysal Hossain <foysalhossain772@gmail.com>
2023-05-04 18:25:19 +0600

    cors policy added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+const allChefs = require('./data/chefs.json')
 app.get('/', (req, res) => {
     res.json({ message: 'hello server are you ok?' })
 })
 
 app.get('/allData', (req, res) => {
-    res.send({ result: 'okkkk' })
+    res.send(allChefs)
 })
 
 app.listen(port, () => {
